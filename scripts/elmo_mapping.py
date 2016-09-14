d93237586c69349eba91aee25d192cffe27dab7d
Mathias Lüdtke <mathias.luedtke@ipa.fraunhofer.de>
2016-09-14 14:28:10 +0200

    added entry for (optional) actual_rated_current

--- scripts/elmo_mapping.py
+++ scripts/elmo_mapping.py
@@ -7,6 +7,7 @@ PDOs={
 
 "TPDO1": [1, ('6041',     "status_word",                  2), ('6061',     "op_mode_display",              1), ('2041',     "timestamp",     4)],
 "TPDO3": [1, ('6064',     "actual_position",              4), ('606C',     "actual_velocity",              4)],
+"TPDO4": [1, ('6078',     "actual_rated_current",         2)],
 }
 
 if __name__ == "__main__":
